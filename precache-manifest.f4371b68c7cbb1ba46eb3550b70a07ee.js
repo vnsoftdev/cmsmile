d33f2a5061cdeb371a981dc8ee96404406a5f9a5
Man Lu <manlu@RLMac1517.local>
2019-05-24 20:12:53 +0700

    Updates

---
+++ precache-manifest.f4371b68c7cbb1ba46eb3550b70a07ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ef01e94d1adb8ee7e15",
+    "url": "/cmsmile/static/css/main.32d7a6b3.chunk.css"
+  },
+  {
+    "revision": "7ef01e94d1adb8ee7e15",
+    "url": "/cmsmile/static/js/main.c3b022b9.chunk.js"
+  },
+  {
+    "revision": "005eb645fa8678f33887",
+    "url": "/cmsmile/static/js/runtime~main.fbf779a7.js"
+  },
+  {
+    "revision": "7b61233e9c5c4142216b",
+    "url": "/cmsmile/static/css/2.ee606d8b.chunk.css"
+  },
+  {
+    "revision": "7b61233e9c5c4142216b",
+    "url": "/cmsmile/static/js/2.0e9a85d5.chunk.js"
+  },
+  {
+    "revision": "53d39311bb9d87c6fa4ae44129cb6c23",
+    "url": "/cmsmile/static/media/comay_logo.53d39311.png"
+  },
+  {
+    "revision": "4be3f8beed94344ed63386b8273fd03c",
+    "url": "/cmsmile/static/media/1.4be3f8be.png"
+  },
+  {
+    "revision": "8c0b4eb47f6e6c943fa8f2a24792f01d",
+    "url": "/cmsmile/static/media/2.8c0b4eb4.png"
+  },
+  {
+    "revision": "0debeb45a06f47bf572fbd393cd71bae",
+    "url": "/cmsmile/static/media/3.0debeb45.png"
+  },
+  {
+    "revision": "70d9f588cdf493c8e46a98f493ac57e8",
+    "url": "/cmsmile/static/media/4.70d9f588.png"
+  },
+  {
+    "revision": "6a5fbc95abff89cd31b35c259a3898c0",
+    "url": "/cmsmile/static/media/5.6a5fbc95.png"
+  },
+  {
+    "revision": "3b91de77a858e756e03dc50fdfd01c26",
+    "url": "/cmsmile/index.html"
+  }
+];
